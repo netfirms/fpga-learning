2e2e33d7f7ad569f0befbcaf0ecf4e49c29c6e88
Taweechai Maklay <m.taweechai@gmail.com>
2026-02-14 16:50:54 +0700

    Update SwitchDrivesLED exteindg logic to 4 switches/LEDs and add project files, configure .gitignore

--- lesson_1_LED/SwitchDrivesLED.vhd
+++ lesson_1_LED/SwitchDrivesLED.vhd
@@ -5,7 +5,13 @@ entity SwitchDrivesLED is
 port
 (
 	sw1:in std_logic;
-	led1:out std_logic
+	led1:out std_logic;
+	sw2:in std_logic;
+	led2:out std_logic;
+	sw3:in std_logic;
+	led3:out std_logic;
+	sw4:in std_logic;
+	led4:out std_logic
 );
 end entity;
 
@@ -13,4 +19,7 @@ architecture rtl of SwitchDrivesLED is
 
 begin
 	led1<=sw1;
+	led2<=sw2;
+	led3<=sw3;
+	led4<=sw4;
 end rtl;
